fa00d799af48772a93a94d5498d7eea47cd8d3be
Carlos Aguilar <caguilar@dwdandsolutions.com>
2023-02-09 22:08:13 -0500

    Fix import
    Signed-off-by: Carlos Aguilar <caguilar@dwdandsolutions.com>

--- lib/src/printer_bluetooth_manager.dart
+++ lib/src/printer_bluetooth_manager.dart
@@ -7,7 +7,7 @@
  */
 
 import 'dart:async';
-import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:rxdart/rxdart.dart';
 import './enums.dart';
 
